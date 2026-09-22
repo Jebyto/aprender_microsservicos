52991eeb5a74a0db108f6c3450f8516dedfa69a0
Jose Carlos <48320837+Jebyto@users.noreply.github.com>
2026-09-22 14:56:03 -0300

    feat(eventos): adicionar exceção para evento não encontrado

---
+++ src/main/java/com/example/demo/exceptions/EventNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.demo.exceptions;
+
+public class EventNotFoundException extends RuntimeException {
+
+    public EventNotFoundException() {
+        super("Evento não encontrado");
+    }
+
+    public EventNotFoundException(String message) {
+        super(message);
+    }
+}
